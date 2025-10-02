b565e22fc46eaab2cbb8a4e5781636ec3ac5eae6
Ushi54 <regile.hidaka@gmail.com>
2025-10-02 22:23:01 +0900

    💄 ログイン、アカウント作成画面のUI作成

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import styles from "./page.module.css";
+import styles from "./../page.module.css";
 import { Box, Button, InputAdornment, Stack, TextField, Typography } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 import LockIcon from '@mui/icons-material/Lock';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -37,14 +38,26 @@ export default function Page() {
       {/* ==========   入力欄  ========== */}
       {/* =============================== */}
 
-      <Typography color="#fff" fontSize={24} mb={-10}>Login</Typography>
+      <Typography color="#fff" fontSize={24} mb={-10}>Create Account</Typography>
 
-      <Stack direction={'column'} mb={14} gap={5}>
-        <TextField id="outlined-userId" label="ユーザID" slotProps={{
+      <Stack direction={'column'} mb={14} gap={1}>
+        <TextField id="outlined-userName" label="アカウント名" slotProps={{
           input: {
             startAdornment: (
               <InputAdornment position="start">
                 <AccountCircle />
+              </InputAdornment>
+            ),
+          },
+        }}
+          variant="filled" sx={{
+            bgcolor: '#fff', width: '300px', borderRadius: 2, height: '50px'
+          }} />
+        <TextField id="outlined-userId" label="ユーザID" slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <AlternateEmailIcon />
               </InputAdornment>
             ),
           },
@@ -64,16 +77,28 @@ export default function Page() {
           variant="filled" type="password" sx={{
             bgcolor: '#fff', width: '300px', borderRadius: 2, height: '50px'
           }} />
+        <TextField id="outlined-rePassWord" label="パスワード再入力" slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+          },
+        }}
+          variant="filled" type="rePassword" sx={{
+            bgcolor: '#fff', width: '300px', borderRadius: 2, height: '50px'
+          }} />
       </Stack>
 
       {/* =============================== */}
       {/* ==========   ボタン  ========== */}
       {/* =============================== */}
       <Stack direction={'row'} gap={7}>
-        <Button onClick={() => router.push("/createAccount")} sx={{ bgcolor: '#fff', color: '#000', width: '120px' }}>
-          アカウント作成
+        <Button onClick={() => router.push("/.")} sx={{ bgcolor: '#fff', color: '#000', width: '120px' }}>
+          戻る
         </Button>
-        <Button sx={{ bgcolor: '#fff', color: '#000', width: '120px' }}>ログイン</Button>
+        <Button sx={{ bgcolor: '#fff', color: '#000', width: '120px' }}>作成する</Button>
       </Stack>
     </div >
   );
